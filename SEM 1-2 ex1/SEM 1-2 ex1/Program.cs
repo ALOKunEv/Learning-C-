52ef6300d065ff52b9b65690e1a636f4a5278db4
Okun <okun@example.com>
2025-10-30 18:51:08 +0300

    Сем1 задания 1, 2, 3

---
+++ SEM 1-2 ex1/SEM 1-2 ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SEM_1_2_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0, c = 0;
+            Console.WriteLine("Введите длины треугольника: ");
+            do
+            {
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    b = Convert.ToDouble(Console.ReadLine());
+                    c = Convert.ToDouble(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Некорректные данные");
+                }
+            } while ((a > b + c) || (b > a + c) || (c > a + b));
+            double per = (a + b + c) / 2;
+            double area = Math.Sqrt(per * (per - a) * (per - b) * (per - c));
+            Console.WriteLine("Площадь треугольника с данными сторонами: ");
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ SEM 1-2 ex3/SEM 1-2 ex3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SEM_1_2_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
