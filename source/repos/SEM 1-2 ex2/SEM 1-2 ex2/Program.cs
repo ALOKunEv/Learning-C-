f29396344613db529550a9699bfdfbc7898f5328
Okun <okun@example.com>
2025-10-30 18:41:42 +0300

    Сем1 задания 1, 2, 3

---
+++ source/repos/SEM 1-2 ex2/SEM 1-2 ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SEM_1_2_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            do
+            {
+                Console.WriteLine("Введите трехзначное число, которое хотите перевернуть: ");
+                try
+                {
+                    num = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException) { Console.WriteLine("Требуется ввести значение типа int"); }
+            } while (!((num > 99) && (num < 1000)));
+
+            int rnum = 0;
+            while (num != 0)
+            {
+                rnum = rnum * 10 + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine("Перевернутое число:");
+            Console.WriteLine(rnum);
+        }
+    }
+}
