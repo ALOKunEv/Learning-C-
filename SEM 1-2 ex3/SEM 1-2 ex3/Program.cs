e5e6c667b1e618dfc9422fca4b6db8f43b56fc42
Okun <okun@example.com>
2025-10-31 13:28:15 +0300

    Сем 1-2 задания 1, 2, 3

--- SEM 1-2 ex3/SEM 1-2 ex3/Program.cs
+++ SEM 1-2 ex3/SEM 1-2 ex3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int f;
+            char a = '.';
+            while (true)
+            {
+                Console.WriteLine("Введите значение переменной a:");
+                try
+                {
+                    a = char.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Надо ввести значение типа char");
+                }
+
+
+
+                if (a == ' ') break;
+            }
         }
     }
 }
