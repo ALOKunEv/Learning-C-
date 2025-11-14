7728dc397f4bcea0bcd8b8e94d05e16fcce4f812
Okun <okun@example.com>
2025-11-14 14:05:58 +0300

    Sem 1-3 ex3

---
+++ Sem 3-4 ex 3/Sem 3-4 ex 3/MathExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sem_3_4_ex_3
+{
+    static class MathExtensions
+    {
+        static public double StandardDeviation(int[] numbers,bool useSampleVariance = false)
+        { 
+            if (numbers == null || numbers.Length == 0) throw new ArgumentException("Массив не должен быть пустым!!!");
+            double mathExpectation = numbers.Average();
+            double sumOfSquaredDeviations = 0;
+            foreach (int num in numbers)
+                sumOfSquaredDeviations += (num - mathExpectation) * (num - mathExpectation);
+            return sumOfSquaredDeviations / numbers.Length;
+
+        }
+
+        static public bool TryCalculateCircle(in double radius, 
+                                            out double area, 
+                                            out double length)
+        {
+            if (radius < 0) 
+                throw new ArgumentException("Радиус должен быть неотрицательным!!!");
+            area = radius * radius * Math.PI;
+            length = radius * Math.PI;
+            return true;
+        }
+
+        static public void SwapValues<T>(ref T leftOp, ref T rightOp)
+        {
+            T temp = leftOp;
+            leftOp = rightOp;
+            rightOp = temp;
+        }
+
+
+    }
+}
---
+++ Sem 3-4 ex 3/Sem 3-4 ex 3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sem_3_4_ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathExtensions.StandardDeviation([1, 2, 3]));
+            int x = 1;
+            int y = MathExtensions.TryCalculateCircle(x);
+            Console.WriteLine($"x = {x}\n y = {y}");
+        }
+    }
+}
